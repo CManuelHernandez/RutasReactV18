646ea35a74e847cafe8dceaa7a730181c51c590c
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-06-23 17:27:37 +0200

    useParams

--- src/componentes/Persona.js
+++ src/componentes/Persona.js
@@ -2,13 +2,18 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 export const Persona = () => {
-  const { nombre, apellido } = useParams();
+  // Valores por defecto en parametros opciones
+  // const { nombre = "UnNamed", apellido = "UnSurNamed" } = useParams();
 
+  const { nombre, apellido } = useParams();
   return (
     <div>
-      <h1>
-        Página de Persona: {nombre} {apellido}
-      </h1>
+      {!nombre && <h1>No hay ninguna persona que mostrar</h1>}
+      {nombre && (
+        <h1>
+          Página de Persona: {nombre} {apellido}
+        </h1>
+      )}
       <p>Esta es la página de Persona</p>
     </div>
   );
