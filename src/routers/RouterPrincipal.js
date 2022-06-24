7df4cabf635793cca7065373917a4be0afb56b0c
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-06-24 10:31:58 +0200

    SubRutas y rutas anidadas

--- src/routers/RouterPrincipal.js
+++ src/routers/RouterPrincipal.js
@@ -11,6 +11,11 @@ import { Articulos } from "../componentes/Articulos";
 import { Contacto } from "../componentes/Contacto";
 import { Error } from "../componentes/Error";
 import { Inicio } from "../componentes/Inicio";
+import { Acerca } from "../componentes/panel/Acerca";
+import { Crear } from "../componentes/panel/Crear";
+import { Gestion } from "../componentes/panel/Gestion";
+import { InicioPanel } from "../componentes/panel/InicioPanel";
+import { PanelControl } from "../componentes/PanelControl";
 import { Persona } from "../componentes/Persona";
 
 export const RouterPrincipal = () => {
@@ -45,6 +50,14 @@ export const RouterPrincipal = () => {
               Contact
             </NavLink>
           </li>
+          <li>
+            <NavLink
+              to="/panel"
+              className={({ isActive }) => (isActive ? "activado" : "")}
+            >
+              Panel de control
+            </NavLink>
+          </li>
         </ul>
       </nav>
 
@@ -64,6 +77,13 @@ export const RouterPrincipal = () => {
             path={"/redirigir"}
             element={<Navigate to="/persona/man/hernandez" />}
           />
+          <Route path="/panel/*" element={<PanelControl />}>
+            <Route index element={<InicioPanel />} />
+            <Route path="inicio" element={<InicioPanel />} />
+            <Route path="crear-articulos" element={<Crear />} />
+            <Route path="gestion-usuarios" element={<Gestion />} />
+            <Route path="acerca-de" element={<Acerca />} />
+          </Route>
           <Route path="*" element={<Error />} />
         </Routes>
       </section>
